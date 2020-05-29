ebf300f48657870637855d4881fcddb000ed0ee6
leremyuan <leremyuan@gmail.com>
2020-05-29 18:14:16 +0800

    添加repl功能

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"monkey-interpreter/repl"
+	"os"
+	user2 "os/user"
+)
+
 func main() {
-	
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	user, err := user2.Current()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	log.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
 }
