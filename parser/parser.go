1805f9c05a31250e44a3178ea407d4a52c741718
leremyuan <leremyuan@gmail.com>
2020-06-01 17:15:33 +0800

    增加parser，并parse let statement。

---
+++ parser/parser.go
@@ -0,0 +1,111 @@
+package parser
+
+import (
+	"fmt"
+	"monkey-interpreter/ast"
+	"monkey-interpreter/lexer"
+	"monkey-interpreter/token"
+)
+
+type Parser struct {
+	l      *lexer.Lexer
+	errors []string
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekErrir(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	//默认当前token为let类型
+	stmt := &ast.LetStatement{Token: p.curToken}
+	//验证下一个token是否为ident，如果是，在expectPeek方法内部会将更新持有的token，
+	//忽略掉curtoken，将下一个ident token更新到curtoken
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+	//形如 let five = 5; 的带么 在identifier之后必然有一个assign类型的token，不然在视为非法
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	//在 assigen 后 ; 之前，都视为expression
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+//验证下一个token是否是期望的token类型，如果是则忽略掉curtoken并更新parser自身持有的token
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekErrir(t)
+		return false
+	}
+}
